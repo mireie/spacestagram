24f610e8fda2bff040de953ae3b31b85a90a1b8e
mireie <michael.reiersgaard@gmail.com>
2021-09-21 15:19:58 -0700

    add post limits and ordering

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,12 @@ class PostsController < ApplicationController
   require "post"
 
   def index
-    if Post.all.size < 20
+    if Post.all.size == 0
       self.create_posts
+    else
+      @posts = Post.all.order(created_at: :desc) 
+      render :index
     end
-    @posts = Post.all
-    render :index
   end
 
   def show
@@ -15,14 +16,17 @@ class PostsController < ApplicationController
   end
 
   def create_posts
-    response = Apod.get_posts(3)
-    response.each do |post|
-      if post.key?("copyright")
-        copyright = post.fetch("copyright")
-      else
-        copyright = ""
+    if Post.all.size <= 60
+      response = Apod.get_posts(20)
+      response.each do |post|
+        if post.key?("copyright")
+          copyright = post.fetch("copyright")
+        else
+          copyright = ""
+        end
+        Post.create(title: post.fetch("title"), image: post.fetch("url"), description: post.fetch("explanation"), copyright: copyright, date: post.fetch("date"))
       end
-      Post.create(title: post.fetch("title"), image: post.fetch("url"), description: post.fetch("explanation"), copyright: copyright, date: post.fetch("date"))
     end
+    redirect_to "/"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts do
     resources :likes
+    collection do
+      post :create_posts
+    end
   end
 
-  root to: 'posts#index'
+  root to: "posts#index"
 end
