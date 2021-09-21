ab6cea5bfd5607b071fdc2f85fae1613aec9f28d
mireie <michael.reiersgaard@gmail.com>
2021-09-21 10:53:19 -0700

    add post show, update styles

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,24 @@
 class LikesController < ApplicationController
   before_action :find_post
+
   def create
-    @post.likes.create(user_id: current_user.id)
-    redirect_to post_path(@post)
+    if liked?
+      flash[:notice] = "You can't like more than once"
+    else
+      flash[:notice] = "Post Liked!"
+      @post.likes.create(user_id: current_user.id)
+    end
+    redirect_to "/"
   end
 
   private
   
   def find_post
     @post = Post.find(params[:post_id])
+  end
+
+  def liked?
+    Like.where(user_id: current_user.id, post_id:
+    params[:post_id]).exists?
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
-  require 'post'
+  require "post"
 
   def index
-    self.create_posts
+    if Post.all.size < 20
+      self.create_posts
+    end
     @posts = Post.all
     render :index
   end
@@ -14,16 +16,8 @@ class PostsController < ApplicationController
 
   def create_posts
     response = Apod.get_posts(3)
-    response.each do |post| 
-      Post.create(title: post.fetch("title"), image: post.fetch("url"), description: post.fetch('explanation'))
+    response.each do |post|
+      Post.create(title: post.fetch("title"), image: post.fetch("url"), description: post.fetch("explanation"))
     end
-    # if @response.response.code === '201'
-    #   flash[:notice] = "#{JSON.parse(@response.body).fetch("name")} Post was successfully created."
-    #   redirect_to("/posts/#{JSON.parse(@response.body).fetch("id").to_s}")
-    # else
-    #   flash[:alert] = @response.fetch("message")
-    #   render :new
-    # end
   end
-
 end
