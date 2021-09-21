4c07a8f279ce73c794a3daf3064348cb82ba7de6
mireie <michael.reiersgaard@gmail.com>
2021-09-21 09:02:37 -0700

    update to create instances of Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,54 +1,29 @@
 class PostsController < ApplicationController
   require 'post'
-  
+
   def index
-    response = Apod.get_posts(3)
-    # need to get only media type images
-    @posts = response
+    self.create_posts
+    @posts = Post.all
     render :index
   end
 
   def show
-    find_me = params[:id]
-    @post = Post.get_posts("posts/#{find_me}")
-    @post_reviews = Post.get_posts_reviews(find_me)
+    @post = Post.find(params[:id])
     render :show
   end
 
-  def create
-    @response = Post.post_post(params)
-    if @response.response.code === '201'
-      flash[:notice] = "#{JSON.parse(@response.body).fetch("name")} Post was successfully created."
-      redirect_to("/posts/#{JSON.parse(@response.body).fetch("id").to_s}")
-    else
-      flash[:alert] = @response.fetch("message")
-      render :new
+  def create_posts
+    response = Apod.get_posts(3)
+    response.each do |post| 
+      Post.create(title: post.fetch("title"), image: post.fetch("url"), description: post.fetch('explanation'))
     end
+    # if @response.response.code === '201'
+    #   flash[:notice] = "#{JSON.parse(@response.body).fetch("name")} Post was successfully created."
+    #   redirect_to("/posts/#{JSON.parse(@response.body).fetch("id").to_s}")
+    # else
+    #   flash[:alert] = @response.fetch("message")
+    #   render :new
+    # end
   end
 
-  def destroy
-    @response = Post.destroy_post(params)
-    if @response.code === 200
-      flash[:notice] = @response.fetch("message")
-      redirect_to ('/')
-    else
-      render :show
-    end
-  end
-
-  def edit
-    find_me = params[:id]
-    @post = Post.get_posts("posts/#{find_me}")
-    render :edit
-  end
-
-  def update 
-    @response = Post.update_post(params)
-    if @response.code === 200
-      flash[:notice] = @response.fetch("message")
-      redirect_to ('/')
-    else
-      render :show
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts do
+    resources :likes
+  end
 
   root to: 'posts#index'
 end
